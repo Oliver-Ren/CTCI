35045dde8a8dfb13230102f02082c8fa4822404f
cr486 <cr486@cornell.edu>
2015-01-07 20:25:57 -0500

    Added the C version.

---
+++ C/solution_1_6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void rotation(int ** image){
+	int length = sizeof(image[])/sizeof(int);
+}
+	
+int main(int argc, char * argv[]){
+	int image[3][3] = {{1,1,1},{1,1,1},{1,1,1}};
+	printf("%d \n",sizeof(image[]));
+	printf("Hello, world. \n");
+	return 0;
+}
+
+	
