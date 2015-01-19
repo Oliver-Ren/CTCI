14e6d133ee517573bfe114785c41ab6356f2de88
Oliver-Ren <cr486@cornell.edu>
2015-01-19 12:41:49 -0500

    Chapter2_6 ptr Based
    Implemented and test the pointer base loop start finding algorithm

--- Java/Chapter 2/Question2_6/Solution.java
+++ Java/Chapter 2/Question2_6/Solution.java
@@ -20,15 +20,43 @@ public class Solution {
 		return null;
 	}
 	
+	public static LinkedListNode findLoopStartPtr( LinkedListNode head){
+		if ( head == null || head.next == null) return null;
+		LinkedListNode ptr1 = head;
+		LinkedListNode ptr2 = head;
+		do {
+			ptr1 = ptr1.next;
+			ptr2 = ptr2.next.next;
+		} while ( ptr1 != ptr2 && ptr2 != null && ptr2.next != null);
+		
+		if ( ptr2 == null || ptr2.next == null) return null;
+		else{
+			ptr2 = head;
+			while ( ptr2 != ptr1){
+				ptr1 = ptr1.next;
+				ptr2= ptr2.next;
+			}
+		}
+		return ptr1;
+	}
+	
 	public static void main(String args[]){
 		LinkedListNode aa = new LinkedListNode(1,null,null);
 		LinkedListNode start = aa;
 		LinkedListNode loopStart = null;
-		LinkedListNode bb = new LinkedListNode(2,null,null);
-
-
-		aa.next = loopStart;
-		System.out.println(start.printForward());
+		LinkedListNode detectedLoopStart = null;
+		LinkedListNode detectedLoopStart2 = null;
+		for (int i = 2; i < 100; i++){
+			LinkedListNode bb = new LinkedListNode(i,null,null);
+			aa.next = bb;
+			aa = aa.next;
+			if (aa.data == 40) loopStart = aa;
+		}
+		//aa.next = loopStart;
+		detectedLoopStart = findLoopStartHash(start);
+		detectedLoopStart2 = findLoopStartPtr(start);
+		System.out.println(detectedLoopStart);
+		System.out.println(detectedLoopStart2);
 	}
 
 }
